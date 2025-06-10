3d99863337b45becb82dc53f08417cb3bc51bf7c
evelynmar2801 <evelzamano@gmail>
2025-06-10 00:18:46 -0600

    Primer commit: Proyecto Angular con .gitignore

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
+import { Products } from './pages/products/products';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },  // Página principal
+  { path: 'nosotros', component: About },
+  { path: 'productos', component: Products },
+  { path: 'contacto', component: Contact },
+  { path: '**', redirectTo: '' },  // Redirección para rutas no existentes
+];
